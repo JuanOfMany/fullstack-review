e1800b27ca1e9245b227313539d527d4c6c79b68
Juan Pinol <JuannnCodes@gmail.com>
2022-06-28 15:59:30 -0400

    Implemented on page load rendering.

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 
 const RepoList = (props) => {
-  console.log(props)
-  let repoList = props.repos.map((repo) =>
-  <li>
-    <div><a href={repo.url}>{repo.name}</a></div>
-    <div>{repo.owner}</div>
-  </li>)
+  let wholeList = props.repos
+  let top25 = wholeList.slice(0,25)
+  let repoList = top25.map((repo) =>
+  <tr key={repo._id}>
+    <td><a href={repo.url} target='_blank'>{repo.name}</a></td>
+    <td>{repo.description}</td>
+    <td>{repo.owner}</td>
+    <td>{repo.stars}</td>
+    <td>{repo.forks}</td>
+  </tr>)
   return (
   <div>
-    <h4> Repo List Component </h4>
-    <ul>{repoList}</ul>
     There are {props.repos.length} repos.
+    <h4> Repo List Component </h4>
+    <table>
+        <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Owner</th>
+          <th>Stars</th>
+          <th>Forks</th>
+        </tr>
+      {repoList}
+      </tbody>
+    </table>
   </div>
   )
   }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,35 +13,56 @@ class App extends React.Component {
     this.search = this.search.bind(this);
   }
 
+  componentDidMount() {
+    let appInstance = this;
+    $.ajax({
+    url: '/repos',
+    type: 'GET',
+    success: function(result) {
+      console.log('Retrieved Repos!')
+      appInstance.setState({repos: JSON.parse(result)})
+    }
+  })}
+
   search (term) {
     let appInstance = this;
-    console.log(`${term} was searched`);
-    $.ajax({
-      url: '/repos',
-      type: 'POST',
-      data: {
-        term: term
-      },
-      success: function (result) {
-        console.log('Successfully Posted!')
-        $.ajax({
-          url: '/repos',
-          type: 'GET',
-          success: function(result) {
-            console.log('Retrieved Repos!')
-            appInstance.setState({repos: JSON.parse(result)})
-          }
-        })
-
-      }
-    });
+    if(term.length === 0) {
+      $.ajax({
+        url: '/repos',
+        type: 'GET',
+        success: function(result) {
+          console.log('Retrieved Repos!')
+          appInstance.setState({repos: JSON.parse(result)})
+        }
+      })
+    } else {
+      console.log(`${term} was searched`);
+      $.ajax({
+        url: '/repos',
+        type: 'POST',
+        data: {
+          term: term
+        },
+        success: function (result) {
+          console.log('Successfully Posted!')
+          $.ajax({
+            url: '/repos',
+            type: 'GET',
+            success: function(result) {
+              console.log('Retrieved Repos!')
+              appInstance.setState({repos: JSON.parse(result)})
+            }
+          })
+        }
+      });
+    }
   }
 
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
       <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos}/>
     </div>)
   }
 }
